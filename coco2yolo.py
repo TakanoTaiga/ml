00cc81c2b51e8bd956408a1edfd61d279a2c34a8
Taiga Takano <ttttghghnb554z@outlook.jp>
2024-04-02 10:46:14 +0900

    update

--- coco2yolo.py
+++ coco2yolo.py
@@ -1,9 +1,12 @@
 import json
 import os
+import shutil
+import random
+
 
 class COCO2YOLO:
-    def __init__(self, json_file):
-        self._check_file_and_dir(json_file, "./out_label")
+    def __init__(self, json_file, out_dir):
+        self._check_file_and_dir(json_file, out_dir)
         self.labels = json.load(open(json_file, 'r', encoding='utf-8'))
         self.coco_id_name_map = self._categories()
         self.coco_name_list = list(self.coco_id_name_map.values())
@@ -78,7 +81,7 @@ class COCO2YOLO:
                 f.write(cls + '\n')
         f.close()
 
-    def coco2yolo(self):
+    def coco2yolo(self, out_dir):
         print("loading image info...")
         images_info = self._load_images_info()
         print("loading done, total images", len(images_info))
@@ -88,13 +91,13 @@ class COCO2YOLO:
         print("converting done, total labels", len(anno_dict))
 
         print("saving txt file...")
-        self._save_txt(anno_dict)
+        self._save_txt(anno_dict, out_dir)
         print("saving done")
 
-    def _save_txt(self, anno_dict):
+    def _save_txt(self, anno_dict, out_dir):
         for k, v in anno_dict.items():
             file_name = os.path.splitext(v[0][0])[0] + ".txt"
-            with open(os.path.join("./out_label", file_name), 'w', encoding='utf-8') as f:
+            with open(os.path.join(out_dir, file_name), 'w', encoding='utf-8') as f:
                 print(k, v)
                 for obj in v:
                     cat_name = self.coco_id_name_map.get(obj[1])
@@ -107,11 +110,41 @@ class COCO2YOLO:
 
 if __name__ == '__main__':
     source_dir = "./input_label"
+    output_dir = './out_label'
+
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
     files = os.listdir(source_dir)
-    print(files)
+
     for file in files:
         file_path = os.path.join(source_dir, file)
         if ".folder_ml" in file_path:
             continue
-        c2y = COCO2YOLO(file_path)
-        c2y.coco2yolo()
+        c2y = COCO2YOLO(file_path, output_dir)
+        c2y.coco2yolo(output_dir)
+
+    output_yolo_dir = './yolo_dataset'
+
+    if not os.path.exists(output_yolo_dir):
+        os.makedirs(output_yolo_dir)
+
+        os.makedirs(output_yolo_dir + "/images/train")
+        os.makedirs(output_yolo_dir + "/images/val")
+        os.makedirs(output_yolo_dir + "/labels/train")
+        os.makedirs(output_yolo_dir + "/labels/val")
+
+    files = os.listdir(output_dir)
+    for file in files:
+        label_file_path = os.path.join(output_dir, file)
+        image_file_path = os.path.join('./out_image', file.replace(".txt",".jpg"))
+        if random.uniform(0, 100) > 20:
+            shutil.copy(label_file_path, output_yolo_dir + "/labels/train")
+            shutil.copy(image_file_path, output_yolo_dir + "/images/train")
+        else:
+            shutil.copy(label_file_path, output_yolo_dir + "/labels/val")
+            shutil.copy(image_file_path, output_yolo_dir + "/images/val")
+    
+    
+
+    
